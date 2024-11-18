ec270215e7dfe907f12b9c21e393cfd00df4436e
Austin <austineisaiahw@gmail.com>
2024-11-18 09:41:00 +0300

    Initial commit-add HTML,CSS and JS files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to change the background color
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "#FF5733"; // Simple color change
+  }
+  
+  // Event listener for the button click
+  document.getElementById('colorButton').addEventListener('click', changeBackgroundColor);
+  
